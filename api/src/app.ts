db37cb0a91f6a25e72c52fdd5276646c020690e6
moto-2525 <frei.vogel.lmk2.li@gmail.com>
2025-11-12 15:43:08 +0900

    4-3MUST課題1提出

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express';
 
 import transactionRouter from "./router/transactions";
 import summaryRouter from "./router/summary";
+import logger from './context/logger';
+import morgan from "morgan";
 
 const app = express();
 const port = 4000;
@@ -10,6 +12,10 @@ const port = 4000;
 // JSONリクエストの解析
 app.use(express.json());
 app.use(cors());
+
+// morgan のログを winston を経由して出力する
+// combined フォーマットで出力し、ログレベルは info に設定
+app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
 
 // /transactions の担当者としてルーターを登録
 app.use("/transactions", transactionRouter);
@@ -20,5 +26,9 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
+  logger.debug("Debug Message");
+  logger.info("Info Message");
+  logger.warn("Warn Message");
+  logger.error("Error Message");
   console.log(`Server running on http://localhost:${port}`);
 });
