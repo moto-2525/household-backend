0dc15bca02d810245608dc4155a8174f8474a2f4
moto-2525 <frei.vogel.lmk2.li@gmail.com>
2025-11-21 14:16:12 +0900

    e2e_test_scenario作成&,e2e_test実行

--- tests/household.spec.ts
+++ tests/household.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect } from "@playwright/test";
 
 test("一覧が表示される", async ({ page }) => {
-  // フロントのURLにアクセス
   await page.goto("http://localhost:3000");
 
-  // テーブルの「ID」ヘッダーが見えることを確認
-  await expect(page.getByRole("columnheader", { name: "ID" })).toBeVisible();
+  // ⭐ テーブルの「ID」が表示されていることを確認
+  await expect(page.getByText("ID")).toBeVisible();
 
-  const rows = page.locator("table tbody tr"); //画面上のtableのtbodyのtrを全部探す→ “一覧テーブルの行（レコード）全部”を指すLocatorを取得
-
-  const rowCount = await rows.count();
-  expect(rowCount).toBeGreaterThan(0);
+  // 行数チェック
+  const rows = page.locator("table tbody tr"); // 画面上のtableのtbodyのtrを全部探してねと指示→“一覧テーブルの行（レコード）全部” を指す Locator を取得
+  const rowCount = await rows.count(); // 実際にブラウザ表示されている行数を数える
+  expect(rowCount).toBeGreaterThan(0); // 行数が0より大きい（＝1行以上ある）ことを期待
 });
+
+//一覧テーブルに1行以上のデータが表示されていることを保証するテスト.フロントが API にアクセスできているか, API が正しい JSON を返しているか,フロントがそのデータをレンダリングできているか
